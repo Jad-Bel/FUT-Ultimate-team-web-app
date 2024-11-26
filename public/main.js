80cd5ae0e20f1a03ee3ed532ea8aa1c5a28a8a59
Jad-Bel <jadthegamer06@gmail.com>
2024-11-26 15:41:52 +0100

    fetching for players and filling the search modal when clicking a card

--- public/main.js
+++ public/main.js
@@ -1,6 +1,31 @@
+// let data = JSON.parse(localStorage.getItem("players") || "[]");
+
+// const fetchData = async () => {
+//     if (data.length === 0 && !localStorage.getItem("players_loaded")) {
+//         try {
+//             const response = await axios.get('https://raw.githubusercontent.com/aymanebenhima/FUT-Champ-Ultimate-Team-Assets/refs/heads/main/players.json');
+//             if (response.data && response.data.players) {
+//                 data = response.data.players;
+//                 localStorage.setItem("players", JSON.stringify(data));
+//                 localStorage.setItem("players_loaded", "true");
+//                 console.log(data);
+//             }
+//         } catch (error) {
+//             console.error("Error fetching data:", error);
+//         }
+//     }
+// }
+
+
+// document.addEventListener('DOMContentLoaded', fetchData)
+
+
 const addPlayerBtn = document.getElementById('addBtn');
-const modalField = document.querySelector('.modal');
-const closeModalField = document.querySelector('.close-btn');
+const modalField = document.querySelector('.modalContainer');
+const closeModalField = document.querySelector('#closeAdd');
+const searchPlayerBtn = document.querySelector('.search-modal');
+
+
 
 addPlayerBtn.addEventListener('click', function() {
     modalField.classList.remove('hidden');
@@ -10,4 +35,16 @@ addPlayerBtn.addEventListener('click', function() {
 closeModalField.addEventListener('click', function() {
     modalField.classList.remove('block');
     modalField.classList.add('hidden');
+})
+
+searchPlayerBtn.addEventListener('click', function () {
+
+})
+
+fetch("../players.json")
+.then(response => response.json())
+.then (allPlayers => {
+    const card = document.querySelector('.cardlid')
+
+    card.addEventListener()
 })
